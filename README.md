3e0c760b3847bdb66638ffae7cc5175ff2e9ed3f
Kevin Ochieng <kevincliffo@gmail.com>
2021-07-18 21:06:37 +0300

    First

---
+++ README.md
@@ -0,0 +1,10 @@
+# Kitcheneesta
+
+Initially developed for the 2015 TiE Young Entrepreneurs program, Kitcheneesta is an Android application that allows the Kitcheneesta bakery customers to remotely customise and order cakes for delivery via a simple and clean user interface. 
+
+This project helped me gain familiarity with designing user interfaces, and
+implementing complex layouts, list adapters and GridViews. 
+
+<img src="screenshots/cake_build.png" width=280>
+<img src="screenshots/delivery_pick.png" width=280>
+<img src="screenshots/order_summary.png" width=280>
---
+++ app/src/main/java/com/kitcheneesta/kitcheneesta/HomeActivity.java
@@ -0,0 +1,71 @@
+package com.kitcheneesta.kitcheneesta;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class HomeActivity extends Activity {
+
+    private Button mOrderButton;
+    private Button mNavButton;
+    private Button mPhoneButton;
+    private Button mEmailButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mOrderButton = (Button) findViewById(R.id.orderButton);
+        mOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startOrderActivity();
+            }
+        });
+
+        mEmailButton = (Button) findViewById(R.id.emailButton);
+        mNavButton = (Button) findViewById(R.id.navButton);
+        mPhoneButton = (Button) findViewById(R.id.callButton);
+        mEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //emails kitcheneesta@gmail.com
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"info@heavenlydelights.co.ke"});
+                email.putExtra(Intent.EXTRA_SUBJECT, "Heavenly Delights Support");
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email, "Choose an Email client:"));
+            }
+        });
+
+        mPhoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //calls the phone number
+                Intent intent = new Intent (Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:+254706012355"));
+                startActivity(intent);
+            }
+        });
+
+        mNavButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // navigate to address (random for now)
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:0,0?q=Maryjoy Primary School - Nyali"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void startOrderActivity() {
+        Intent intent = new Intent(this, OrderActivity.class);
+        startActivity(intent);
+    }
+}
